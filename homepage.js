e0e72fff801914f982f1eec1926d31ac74b6c393
username <mohseen_abasi@yahoo.com>
2021-11-01 16:34:25 +0330

    finish responsive homepage

---
+++ homepage.js
@@ -0,0 +1,11 @@
+//  Initialize Swiper
+var swiper = new Swiper('.mySwiper', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false,
+	},
+});
