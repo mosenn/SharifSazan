e988772e527d204b7a33572893ceea235a493a61
username <mohseen_abasi@yahoo.com>
2021-11-03 11:38:54 +0330

    fixed footer

--- homepage.js
+++ homepage.js
@@ -1,9 +1,11 @@
 //  Initialize Swiper
 var swiper = new Swiper('.mySwiper', {
+	slidesPerGroup: 1,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+
 	// autoplay: {
 	// 	delay: 4000,
 	// 	disableOnInteraction: false,
