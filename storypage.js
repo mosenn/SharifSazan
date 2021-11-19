06bc12b20c1fd8ebd8e0ec381867f91743af8650
username <mohseen_abasi@yahoo.com>
2021-11-19 11:02:41 +0330

    edit jome sob

--- storypage.js
+++ storypage.js
@@ -1,6 +1,6 @@
-const scrollContainer = document.querySelector('.main-story-page ');
+const scrollContainer = document.querySelector('.main-story-page');
 
-let MenuPageScorll1 = document.querySelector('.MenuPageScorll');
+// let MenuPageScorll1 = document.querySelector('.MenuPageScorll');
 
 scrollContainer.addEventListener('wheel', (evt) => {
 	evt.preventDefault();
